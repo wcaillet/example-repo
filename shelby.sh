93b864e62c856b243caa0893617275b38c3af76d
Wensie <wcaillet@MacBook-Pro.local>
2016-02-03 13:45:05 -0600

    initial commit

---
+++ shelby.sh
@@ -0,0 +1,8 @@
+mkdir shelby
+cd ./shelby
+touch README fizz.txt buzz.txt deleteme.txt
+echo fizzbuzz > README
+mkdir texts ##Creating a new directory doesn't automatically move you into it
+
+mv *.txt ./texts
+rm deleteme.txt
